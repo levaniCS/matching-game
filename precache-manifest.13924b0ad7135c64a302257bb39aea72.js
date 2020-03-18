76af9b2c6530f9cb53f112e59e0aebc2fe539846
levaniCS <levanisarishvili3105@gmail.com>
2020-03-18 19:45:01 +0400

    Updates

---
+++ precache-manifest.13924b0ad7135c64a302257bb39aea72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a16fcee6d0851ff9dc7c240bceec3848",
+    "url": "/matching-game/index.html"
+  },
+  {
+    "revision": "11452a14320c84964b97",
+    "url": "/matching-game/static/css/main.7e0a7c16.chunk.css"
+  },
+  {
+    "revision": "2a14f2b98651549eeb6a",
+    "url": "/matching-game/static/js/2.e089bb4d.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/matching-game/static/js/2.e089bb4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11452a14320c84964b97",
+    "url": "/matching-game/static/js/main.81d50152.chunk.js"
+  },
+  {
+    "revision": "d316354f329e939ae7f8",
+    "url": "/matching-game/static/js/runtime-main.999d7b1d.js"
+  }
+]);
